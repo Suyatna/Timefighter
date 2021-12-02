02576a7997fd7f65909bf94bdb3492d0939cc94a
Suyatna <zuyatna@gmail.com>
2021-12-02 09:53:00 +0700

    hooking up views

--- app/src/main/java/com/example/timefighter/MainActivity.kt
+++ app/src/main/java/com/example/timefighter/MainActivity.kt
@@ -2,10 +2,49 @@ package com.example.timefighter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var gameScoreTextView: TextView
+    private lateinit var timeLeftTextView: TextView
+    private lateinit var tapMeButton: Button
+
+    private var score = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // connect views to variable
+        gameScoreTextView = findViewById(R.id.game_score_text_view)
+
+        timeLeftTextView = findViewById(R.id.time_left_text_view)
+
+        tapMeButton = findViewById(R.id.tap_me_button)
+
+        // listener from variables
+        tapMeButton.setOnClickListener { incrementStore() }
+    }
+
+    private fun incrementStore() {
+        // increment score logic
+        score++
+
+        val newScore = "Your Score: $score"
+        gameScoreTextView.text = newScore
+    }
+
+    private fun resetGame() {
+        // reset game logic
+    }
+
+    private fun startGame() {
+        // start game logic
+    }
+
+    private fun endGame() {
+        // end game logic
     }
 }
